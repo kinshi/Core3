5e12d22e76d02997e0e11f67c713a6199cbf9894
stacy19201325 <stacy_19201325@hotmail.com>
2017-02-05 18:51:35 -0600

    [Removed] Planet-specific resource requirements from draft schematics

--- MMOCoreORB/bin/scripts/object/draft_schematic/bio_engineer/utilities/pet_vitapack_c.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/bio_engineer/utilities/pet_vitapack_c.lua
@@ -65,7 +65,7 @@ object_draft_schematic_bio_engineer_utilities_pet_vitapack_c = object_draft_sche
    ingredientTemplateNames = {"craft_chemical_ingredients_n", "craft_chemical_ingredients_n", "craft_chemical_ingredients_n", "craft_chemical_ingredients_n"},
    ingredientTitleNames = {"organic_element", "inorganic_element", "delivery_medium", "drug_strength_compound"},
    ingredientSlotType = {0, 0, 1, 1},
-   resourceTypes = {"fruit_flowers_endor", "radioactive_unknown", "object/tangible/component/chemistry/shared_liquid_delivery_suspension_advanced.iff", "object/tangible/component/chemistry/shared_biologic_effect_controller_advanced.iff"},
+   resourceTypes = {"fruit_flowers", "radioactive_unknown", "object/tangible/component/chemistry/shared_liquid_delivery_suspension_advanced.iff", "object/tangible/component/chemistry/shared_biologic_effect_controller_advanced.iff"},
    resourceQuantities = {32, 24, 3, 1},
    contribution = {100, 100, 100, 100},
 
