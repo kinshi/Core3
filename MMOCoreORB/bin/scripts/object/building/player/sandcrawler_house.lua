396e41251fc1e2cfacf79a1c5dd9f804bc1232e0
stacy19201325 <stacy_19201325@hotmail.com>
2017-02-28 21:32:23 -0600

    [Added] House signs to Mustafar bunker and sandcrawler house

--- MMOCoreORB/bin/scripts/object/building/player/sandcrawler_house.lua
+++ MMOCoreORB/bin/scripts/object/building/player/sandcrawler_house.lua
@@ -56,11 +56,18 @@ object_building_player_sandcrawler_house = object_building_player_shared_sandcra
 	},
 	childObjects = {
 --			{templateFile = "object/tangible/sign/player/house_address.iff", x = 4.34, z = 3.4, y = 18.40, ox = 0, oy = 0.707107, oz = 0, ow = 0.707107, cellid = -1, containmentType = -1},
-			{templateFile = "object/tangible/terminal/terminal_player_structure.iff", x = -4.25791, z = 9.29876, y = -13.4098, ow = -0.707107, ox = 0, oz = 0, oy = -0.707107, cellid = 2, containmentType = -1}
+			{templateFile = "object/tangible/terminal/terminal_player_structure.iff", x = -4.25791, z = 9.29876, y = -13.4098, ow = -0.707107, ox = 0, oz = 0, oy = -0.707107, cellid = 2, containmentType = -1},
 --			{templateFile = "object/tangible/terminal/terminal_elevator_up.iff", x = -2.66109, z = -0.513074, y = 0.0544102, ow = -0.707107, ox = 0, oz = 0, oy = 0.707107, cellid = 7, containmentType = -1},
---			{templateFile = "object/tangible/terminal/terminal_elevator_down.iff", x = -2.65908, z = 5.39344, y = -0.0155522, ow = -0.707107, ox = 0, oz = 0, oy = 0.707107, cellid = 7, containmentType = -1}
-	}
-
+--			{templateFile = "object/tangible/terminal/terminal_elevator_down.iff", x = -2.65908, z = 5.39344, y = -0.0155522, ow = -0.707107, ox = 0, oz = 0, oy = 0.707107, cellid = 7, containmentType = -1},
+{templateFile = "object/tangible/sign/player/house_address_corellia.iff", x = -9.5, z = 4, y = 1.5, ox = 0, oy = 0, oz = 0, ow = 1, cellid = -1, containmentType = -1},
+	},
+	shopSigns = {
+		{templateFile = "object/tangible/sign/player/house_address_corellia.iff", x = -9.5, z = 4, y = 1.5, ox = 0, oy = 0, oz = 0, ow = 1, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:house_address"},
+           		 {templateFile = "object/tangible/sign/player/shop_sign_s01.iff", x = -6, z = 0, y = 3.2, ox = 0, oy = 0.707107, oz = 0, ow =  1, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:shop_sign1"},
+			{templateFile = "object/tangible/sign/player/shop_sign_s02.iff", x = -6, z = 0, y = 3.2, ox = 0, oy = -0.707107, oz = 0, ow =  0.707107, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:shop_sign2"},
+			{templateFile = "object/tangible/sign/player/shop_sign_s03.iff", x = -6, z = 0, y = 3.2, ox = 0, oy = 0.707107, oz = 0, ow =  0.707107, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:shop_sign3"},
+			{templateFile = "object/tangible/sign/player/shop_sign_s04.iff", x = -6, z = 0, y = 3.2, ox = 0, oy = 0.707107, oz = 0, ow = 0.707107, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:shop_sign4"},				
+	},
 }
 
 ObjectTemplates:addTemplate(object_building_player_sandcrawler_house, "object/building/player/sandcrawler_house.iff")
