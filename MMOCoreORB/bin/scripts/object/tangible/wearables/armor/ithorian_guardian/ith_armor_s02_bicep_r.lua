07767b00c2a4cb2341be399bc85eef94d032d3bf
stacy19201325 <stacy_19201325@hotmail.com>
2017-03-11 18:24:36 -0600

    [Removed] Special resist from Ith Guardian & Kash Ceremonial armors

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/ithorian_guardian/ith_armor_s02_bicep_r.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/ithorian_guardian/ith_armor_s02_bicep_r.lua
@@ -77,8 +77,8 @@ object_tangible_wearables_armor_ithorian_guardian_ith_armor_s02_bicep_r = object
 	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 	experimentalGroupTitles = {"null", "null", "null", "null", "exp_quality", "exp_durability", "exp_durability", "exp_durability", "exp_durability", "null", "null", "exp_resistance", "null"},
 	experimentalSubGroupTitles = {"null", "null", "sockets", "hit_points", "armor_effectiveness", "armor_integrity", "armor_health_encumbrance", "armor_action_encumbrance", "armor_mind_encumbrance", "armor_rating", "armor_special_type", "armor_special_effectiveness", "armor_special_integrity"},
-	experimentalMin = {0, 0, 0, 1000, 5, 15000, 13, 13, 16, 1, 1, 5, 15000},
-	experimentalMax = {0, 0, 0, 1000, 30, 25000, 8, 8, 9, 1, 1, 40, 25000},
+	experimentalMin = {0, 0, 0, 1000, 1, 30000, 22, 25, 22, 1, 1, 5, 30000},
+	experimentalMax = {0, 0, 0, 5000, 40, 50000, 13, 15, 13, 1, 1, 40, 50000},
 	experimentalPrecision = {0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0},
 	experimentalCombineType = {0, 0, 4, 1, 1, 1, 1, 1, 1, 4, 4, 4, 1},
 }
--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/kashyyykian_ceremonial/armor_kashyyykian_ceremonial_bracer_r.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/kashyyykian_ceremonial/armor_kashyyykian_ceremonial_bracer_r.lua
@@ -77,8 +77,8 @@ object_tangible_wearables_armor_kashyyykian_ceremonial_armor_kashyyykian_ceremon
 	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 	experimentalGroupTitles = {"null", "null", "null", "null", "exp_quality", "exp_durability", "exp_durability", "exp_durability", "exp_durability", "null", "null", "exp_resistance", "null"},
 	experimentalSubGroupTitles = {"null", "null", "sockets", "hit_points", "armor_effectiveness", "armor_integrity", "armor_health_encumbrance", "armor_action_encumbrance", "armor_mind_encumbrance", "armor_rating", "armor_special_type", "armor_special_effectiveness", "armor_special_integrity"},
-	experimentalMin = {0, 0, 0, 1000, 5, 15000, 23, 23, 26, 1, 1, 5, 15000},
-	experimentalMax = {0, 0, 0, 1000, 25, 25000, 18, 18, 19, 1, 1, 40, 25000},
+	experimentalMin = {0, 0, 0, 1000, 1, 30000, 25, 22, 25, 1, 0, 5, 0},
+	experimentalMax = {0, 0, 0, 5000, 40, 50000, 15, 13, 15, 1, 0, 55, 0},
 	experimentalPrecision = {0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0},
 	experimentalCombineType = {0, 0, 4, 1, 1, 1, 1, 1, 1, 4, 4, 4, 1},
 }
